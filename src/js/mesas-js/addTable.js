659c8a10e0ca33aab53955eb1fc8fa4dee2ee48e
Mitzi Osorio <mitzi.rios02@gmail.com>
2023-12-29 13:05:47 -0600

    solucion de bug con iframe ventana mesas -> popup numero de comenales

--- src/js/mesas-js/addTable.js
+++ src/js/mesas-js/addTable.js
@@ -1,10 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-    //Declaracion de variables
     var tableList = document.getElementById("mesaList");
     var addTableButton = document.getElementById("addTable");
     var lastItem = document.getElementById("lastItem");
-
     var modalComensales = document.getElementById("modal-num-comensales");
 
     // Leer mesas almacenadas localmente
@@ -15,11 +12,11 @@ document.addEventListener("DOMContentLoaded", function () {
     function addMesa() {
         var newTable = document.createElement("li");
         newTable.classList.add("table-list__item");
-
         newTable.id = 'table' + tableCounter;
 
         newTable.innerHTML = '<h2 class="table-list__item__title">Mesa ' + tableCounter + '</h2>' +
             '<img class="table-list__item__icon" src="./src/assets/icon/iconos-hra-01.svg" alt="table-icon">';
+        
         tableList.insertBefore(newTable, lastItem);
 
         newTable.addEventListener("click", function () {
@@ -36,23 +33,51 @@ document.addEventListener("DOMContentLoaded", function () {
     // Cargar mesas almacenadas
     function loadStoredTables() {
         for (var i = 0; i < storedTables.length; i++) {
-            var newTable = document.createElement("li");
-            newTable.classList.add("table-list__item");
-            newTable.id = 'table' + storedTables[i].id;
-            newTable.innerHTML = '<h2 class="table-list__item__title">Mesa ' + storedTables[i].id + '</h2>' +
-                '<img class="table-list__item__icon" src="./src/assets/icon/iconos-hra-01.svg" alt="table-icon">';
-            tableList.insertBefore(newTable, lastItem);
+            var existingTable = document.getElementById('table' + storedTables[i].id);
+
+            if (existingTable) {
+                existingTable.addEventListener("click", function () {
+                    showModal();
+                });
+            } else {
+                var newTable = document.createElement("li");
+                newTable.classList.add("table-list__item");
+                newTable.id = 'table' + storedTables[i].id;
+                newTable.innerHTML = '<h2 class="table-list__item__title">Mesa ' + storedTables[i].id + '</h2>' +
+                    '<img class="table-list__item__icon" src="./src/assets/icon/iconos-hra-01.svg" alt="table-icon">';
+                
+                tableList.insertBefore(newTable, lastItem);
+
+                newTable.addEventListener("click", function () {
+                    showModal();
+                });
+            }
         }
     }
 
     // Mostrar modal
     function showModal() {
         modalComensales.style.display = "block";
+
+        
+        var myIframe = document.getElementById('frame-open-table');
+        var doc = myIframe.contentDocument || myIframe.contentWindow.document;
+
+        var confirmar = doc.getElementById("btnCerrarModal");
+
+        confirmar.addEventListener("click", function () {
+            hideModal();
+        })
+    }
+
+    function hideModal() {
+        console.log("btn oprimido");
+        modalComensales.style.display = "none";
     }
 
     // llamar mesas almacenadas
     loadStoredTables();
 
     addTableButton.addEventListener("click", addMesa);
-
 });
+
---
+++ src/js/mesas-js/modal-open-table.js
@@ -0,0 +1,6 @@
+function hideModal() {
+    var modalComensales = document.getElementById("modal-num-comensales");
+    
+    console.log("btn oprimido");
+    modalComensales.style.display = "none";
+}
