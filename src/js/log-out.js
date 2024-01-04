a97c6501cd28739e3101c03c34e61f0d73d53c0e
Mitzi Osorio <mitzi.rios02@gmail.com>
2024-01-04 17:16:29 -0600

    Actualizacion de responsive y areas de mesas. Implementacion de menu responsive

---
+++ src/js/log-out.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+    var logOut = document.getElementById("log-out-btn"); 
+    logOut.addEventListener("click", function () {
+        window.location.href = 'index.html'; 
+    });
+});
--- src/js/mesas-js/addTable.js
+++ src/js/mesas-js/addTable.js
@@ -1,51 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
     var tableList = document.getElementById("mesaList");
-    var addTableButton = document.getElementById("addTable");
     var lastItem = document.getElementById("lastItem");
     var modalComensales = document.getElementById("modal-num-comensales");
 
-    // Leer mesas almacenadas localmente
-    var storedTables = JSON.parse(localStorage.getItem('tables')) || [];
-    var tableCounter = storedTables.length > 0 ? storedTables[storedTables.length - 1].id + 1 : 1;
+    var mesasPorArea = {
+        "Area Principal": 8,
+        Terraza: 5,
+        Salón: 7,
+        Barra: 3
+    };
 
-    // Agregar mesa
-    function addMesa() {
-        var newTable = document.createElement("li");
-        newTable.classList.add("table-list__item");
-        newTable.id = 'table' + tableCounter;
+    // Agregar mesas dinámicamente
+    function loadTables(area) {
+        var numeroDeMesas = mesasPorArea[area];
 
-        newTable.innerHTML = '<h2 class="table-list__item__title">Mesa ' + tableCounter + '</h2>' +
-            '<img class="table-list__item__icon" src="./src/assets/icon/iconos-hra-01.svg" alt="table-icon">';
-        
-        tableList.insertBefore(newTable, lastItem);
+        // Ocultar mesas antes de mostrar area seleccionada
+        hideAllTables();
 
-        newTable.addEventListener("click", function () {
-            showModal();
-        });
-
-        var tableInfo = { id: tableCounter };
-        storedTables.push(tableInfo);
-        localStorage.setItem('tables', JSON.stringify(storedTables));
-
-        tableCounter++;
-    }
-
-    // Cargar mesas almacenadas
-    function loadStoredTables() {
-        for (var i = 0; i < storedTables.length; i++) {
-            var existingTable = document.getElementById('table' + storedTables[i].id);
+        for (var i = 1; i <= numeroDeMesas; i++) {
+            var mesaId = area.replace(/ /g, '-') + '-table' + i;
+            var existingTable = document.getElementById(mesaId);
 
             if (existingTable) {
+                existingTable.style.display = "flex"; // Mostrar la mesa
                 existingTable.addEventListener("click", function () {
                     showModal();
                 });
             } else {
                 var newTable = document.createElement("li");
-                newTable.classList.add("table-list__item");
-                newTable.id = 'table' + storedTables[i].id;
-                newTable.innerHTML = '<h2 class="table-list__item__title">Mesa ' + storedTables[i].id + '</h2>' +
-                    '<img class="table-list__item__icon" src="./src/assets/icon/iconos-hra-01.svg" alt="table-icon">';
-                
+                newTable.classList.add("table-list-item");
+                newTable.id = mesaId;
+                newTable.style.display = "flex"; // Mostrar la nueva mesa
+                newTable.innerHTML = '<h2 class="table-list-item-title">Mesa ' + i + ' - ' + area + '</h2>' +
+                    '<img class="table-list-item-icon" src="./src/assets/icon/iconos-hra-01.svg" alt="table-icon">';
+
                 tableList.insertBefore(newTable, lastItem);
 
                 newTable.addEventListener("click", function () {
@@ -55,11 +43,19 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    // Ocultar todas las mesas
+    function hideAllTables() {
+        var tables = tableList.getElementsByClassName("table-list-item");
+
+        for (var i = 0; i < tables.length; i++) {
+            tables[i].style.display = "none";
+        }
+    }
+
     // Mostrar modal
     function showModal() {
         modalComensales.style.display = "block";
 
-        
         var myIframe = document.getElementById('frame-open-table');
         var doc = myIframe.contentDocument || myIframe.contentWindow.document;
 
@@ -67,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         confirmar.addEventListener("click", function () {
             hideModal();
-        })
+        });
     }
 
     function hideModal() {
@@ -77,9 +73,28 @@ document.addEventListener("DOMContentLoaded", function () {
         window.location.href = 'mesa-abierta.html';
     }
 
-    // llamar mesas almacenadas
-    loadStoredTables();
+    // Event listener para los botones del menú principal
+    var areaPrincipalBtn = document.getElementById("areaPrincipalBtn");
+    var terrazaBtn = document.getElementById("terrazaBtn");
+    var salonBtn = document.getElementById("salonBtn");
+    var barraBtn = document.getElementById("barraBtn");
 
-    addTableButton.addEventListener("click", addMesa);
+    areaPrincipalBtn.addEventListener("click", function () {
+        loadTables("Area Principal");
+    });
+
+    terrazaBtn.addEventListener("click", function () {
+        loadTables("Terraza");
+    });
+
+    salonBtn.addEventListener("click", function () {
+        loadTables("Salón");
+    });
+
+    barraBtn.addEventListener("click", function () {
+        loadTables("Barra");
+    });
+
+    // Llamar mesas dinámicamente inicialmente para el área principal
+    loadTables("Area Principal");
 });
-
