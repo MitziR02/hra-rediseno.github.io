f0d50c808315097ecdcc5836d25593bfc0a5b254
Mitzi Osorio <mitzi.rios02@gmail.com>
2024-01-04 23:39:03 -0600

    modales ventana mesas

--- src/js/mesas-js/modales-table.js
+++ src/js/mesas-js/modales-table.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const juntarMesasBtn = document.getElementById('juntar-mesas');
     const modalJuntarMesas = document.getElementById('modal-juntar-mesas');
-    const btnSubmitMesas = document.getElementById('mesas-confirmar');
+    const btnSubmitMesas = document.getElementById('juntar-confirmar');
 
     juntarMesasBtn.addEventListener("click", ()=>{
         modalJuntarMesas.showModal();
@@ -10,5 +10,32 @@ document.addEventListener("DOMContentLoaded", function () {
     btnSubmitMesas.addEventListener("click", ()=>{
         event.preventDefault();
         modalJuntarMesas.close();
+    });
+
+
+    const btnCambiarMesas = document.getElementById('cambiar-mesas');
+    const modalCambiarMesas = document.getElementById('modal-cambiar-mesa');
+    const btnSubmitCambiar = document.getElementById ('cambiar-confirmar');
+
+    btnCambiarMesas.addEventListener("click", ()=>{
+        modalCambiarMesas.showModal();
+    })
+
+    btnSubmitCambiar.addEventListener("click", ()=>{
+        event.preventDefault();
+        modalCambiarMesas.close();
+    });
+
+    const btnCambiarMesero = document.getElementById('cambiar-mesero');
+    const modalCambiarMesero = document.getElementById('modal-cambiar-mesero');
+    const btnSubmitMesero = document.getElementById ('mesero-confirmar');
+
+    btnCambiarMesero.addEventListener("click", ()=>{
+        modalCambiarMesero.showModal();
+    })
+
+    btnSubmitMesero.addEventListener("click", ()=>{
+        event.preventDefault();
+        modalCambiarMesero.close();
     });
 });
